74dd4c0fcaaa4b69db7c9a51db3bb9e93a65e3bf
Paul <paulrommel87@gmail.com>
2021-09-19 21:24:55 +0300

    Create Table View App

---
+++ MyPlaces/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  MyPlaces
+//
+//  Created by Павел Попов on 19.09.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
