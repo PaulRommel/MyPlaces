c44ba855f653d5a394ccca2e9b02804bb4539f52
Paul <paulrommel87@gmail.com>
2021-10-30 13:37:42 +0300

    17. Refactor Code to Use DB

--- MyPlaces/PlaceModel.swift
+++ MyPlaces/PlaceModel.swift
@@ -15,23 +15,11 @@ class Place: Object {
     @objc dynamic var type: String?
     @objc dynamic var imageData: Data?
     
-    let restaurantNames = ["Dublin", "Марбелья", "Корчма Гоголь", "Merzen", "Шокунин", "ПолМедведя", "Малика", "Иль Песто", "Фабрика", "Фёст", "McDonalds", "KFC"]
-    
-    func savePlaces() {
-        
-        for place in restaurantNames {
-            
-            let image = UIImage(named: place)
-            guard let imageData = image?.pngData() else { return }
-            
-            let newPlace = Place()
-            
-            newPlace.name = place
-            newPlace.location = "Lipetsk"
-            newPlace.type = "Restaurant"
-            newPlace.imageData = imageData
-            
-            StorageManager.saveObject(newPlace)
-        }
+    convenience init(name: String, location: String?, type: String?, imageData: Data?) {
+        self.init()
+        self.name = name
+        self.location = location
+        self.type = type
+        self.imageData = imageData
     }
 }
